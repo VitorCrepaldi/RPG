13db62c347c56b88e5f238be504275b900b2eccc
VitorCrepaldi <vitorcrepaldi28@gmail.com>
2016-07-27 11:37:05 -0300

    Update Personagem.java

--- src/Personagem.java
+++ src/Personagem.java
@@ -52,7 +52,7 @@ public class Personagem {
 	}
 	//---------------------------------------------------------------------
 	
-	//PONTO DE EXPERIÊNCIA----------------------------------------------------------------
+	//PONTO DE EXPERIENCIA----------------------------------------------------------------
 	public void setPe(int pe){
 		this.pe = pe;
 	}
@@ -81,24 +81,33 @@ public class Personagem {
 		return pm;
 	}
 	//----------------------------------------------------------------------
+	
+	//DEFINE A CLASSE QUE O PERSONAGEM VAI SER.
 	public void setClasse(int tipo){
 		switch(tipo){
+			//1 = GUERREIRO
 		case 1: this.classe = new Guerreiro();
 				break;
+			//2 = ARQUEIRO
 		case 2: this.classe = new Arqueiro();
 				break;
+			//3 = MAGO
 		case 3: this.classe = new Mago();
 				break;
+			//4 = MONSTRO				
 		case 4: this.classe = new Monstro();
 				break;
 		}
 	}
+	//PEGA AGILIDADE
 	public int getAgil(){
 		return this.getNivel() * classe.getAgilidade();
 	}
+	//PEGA FORCA
 	public int getForca(){
 		return this.getNivel() * classe.getForca();
 	}
+	//PEGA INTELIGENCIA
 	public int getIntel(){
 		return this.getNivel() * classe.getInteligencia();
 	}
