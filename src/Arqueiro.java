2c4e331a6e693b05657bef85794f908259c87d69
infn3 <ejgsdc@gmail.com>
2016-07-26 21:39:05 -0300

    Update Arqueiro.java

--- src/Arqueiro.java
+++ src/Arqueiro.java
@@ -23,7 +23,7 @@ public class Arqueiro extends Classe {
 	public void setHabilidades(Personagem p){
         habilidades = new Habilidade[3]; //ALVO | TRUE = ALVO UNICO | FALSE = TOOS OS ALVOS
 	    
-//		habilidades[0] = new Habilidade("Flecha Encantada",0.4,0.8,0,5,true,0,0,0);		
+		habilidades[0] = new Habilidade("Flecha Encantada",0.4,0.8,0,5,true,0,0,0,p);		
 		habilidades[1] = new Habilidade("Atirar Flecha",0.5,0.3,0,4,true,0,0,0,p);
 		habilidades[2] = new Habilidade("Socar",0.1,0.3,0,4,true,0,0,0,p);	
 	}
