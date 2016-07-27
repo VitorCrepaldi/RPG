e17943260b4131276b97d4156db10f2f9895cf81
VitorCrepaldi <vitorcrepaldi28@gmail.com>
2016-07-26 21:40:19 -0300

    Update Mago.java

--- src/Mago.java
+++ src/Mago.java
@@ -21,9 +21,9 @@ public class Mago extends Classe {
 	public void setHabilidades(Personagem p){
         habilidades = new Habilidade[3]; //ALVO | TRUE = ALVO UNICO | FALSE = TOOS OS ALVOS
 	    
-//		habilidades[0] = new Habilidade("Flecha Encantada",0.4,0.8,0,5,true,0,0,0);		
-		habilidades[1] = new Habilidade("Atirar Flecha",0.5,0.3,0,4,true,0,0,0,p);
-		habilidades[2] = new Habilidade("Socar",0.1,0.3,0,4,true,0,0,0,p);	
+		habilidades[0] = new Habilidade("Enfraquecer",0.2,0.3,0,5,true,0,0,0);		
+		habilidades[1] = new Habilidade("Cura amigo",0,0,0.8*-1,4,true,0,0,0,p);
+		habilidades[2] = new Habilidade("Socar",0.1,0.1,0,2,true,0,0,0,p);	
 	}
 	public int getTipo(){
 		return 3;
