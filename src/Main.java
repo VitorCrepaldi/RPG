63d34c21257554b0cfbcd11736e86409a92ece6e
natak99 <natakaique@outlook.com>
2016-07-27 22:00:40 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,8 @@
-//import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-//		int resp;
-		Batalha batle = new Batalha();
-		batle.inicio();
-		
-//		System.out.printf("\n *-------------------------------------*");
-//		System.out.printf("\n |                 MENU                |");
-//		System.out.printf("\n *-------------------------------------*");
-//		System.out.printf("\n |   1. NOVO JOGO                      |");
-//		System.out.printf("\n |   2. CONTINUAR                      |");
-//		System.out.printf("\n |   3. LOJA                           |");
-//		System.out.printf("\n |   0. SAIR                           |");
-//		System.out.printf("\n *-------------------------------------*");
-//		 resp=scan.nextInt();
-		 
-//		switch(resp){
-//		case 1:
-//			break;
-//		case 2:
-//			break;
-//		case 3:
-//			break;
-//		case 0:
-//			break;
-//		}
+		Batalha b = new Batalha();
 	}
 
 }
