8053993a4a278c2e04d36b903c43ee657718931e
infn3 <ejgsdc@gmail.com>
2016-07-28 21:41:38 -0300

    Add files via upload

---
+++ src/Resultado.java
@@ -0,0 +1,54 @@
+
+public class Resultado {
+
+	public static void main(String[] args) {
+		int resultado=0;
+		
+		if(resultado==0){
+			
+			System.out.printf("\n ████▀░░░░░░░░░░░░░░░░░▀████  ");
+			System.out.printf("\n ███│░░░░░░░░░░░░░░░░░░░│███   ");
+			System.out.printf("\n ██▌│░░░░░░░░░░░░░░░░░░░│▐██   ");
+			System.out.printf("\n ██░└┐░░░░░░░░░░░░░░░░░┌┘░██   ");
+			System.out.printf("\n ██░░└┐░░░░░░░░░░░░░░░┌┘░░██   ");
+			System.out.printf("\n ██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██   ");
+			System.out.printf("\n ██▌░│██████▌░░░▐██████│░▐██   ");
+			System.out.printf("\n ███░│▐███▀▀░░▄░░▀▀███▌│░███   ");
+			System.out.printf("\n ██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██   ");
+			System.out.printf("\n ██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██   ");
+			System.out.printf("\n ████▄─┘██▌░░░░░░░▐██└─▄████   ");
+			System.out.printf("\n █████░░▐█─┬┬┬┬┬┬┬─█▌░░█████   ");
+			System.out.printf("\n ████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████   ");
+			System.out.printf("\n █████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████   ");
+			System.out.printf("\n ███████▄░░░░░░░░░░░▄███████ ");
+			System.out.printf("\n\n  	 VOCÊ PERDEU!");
+			
+		}else if(resultado==1){
+
+			System.out.printf("\n ___________________*___________________ ");
+			System.out.printf("\n __________________***__________________ ");
+			System.out.printf("\n _________________*****_________________ ");
+			System.out.printf("\n ________________*******________________ ");
+			System.out.printf("\n _______________*********_______________ ");
+			System.out.printf("\n ______________***********______________ ");
+			System.out.printf("\n _____________*************_____________ ");
+			System.out.printf("\n *************************************** ");
+			System.out.printf("\n _*************************************_ ");
+			System.out.printf("\n ___*********************************___ ");
+			System.out.printf("\n _____*****************************_____ ");
+			System.out.printf("\n _______*************************_______ ");
+			System.out.printf("\n _________*********************_________ ");
+			System.out.printf("\n __________*******************__________ ");
+			System.out.printf("\n _________*********************_________ ");
+			System.out.printf("\n ________***********************________ ");
+			System.out.printf("\n _______************_************_______ ");
+			System.out.printf("\n ______***********_____***********______ ");
+			System.out.printf("\n _____*********___________*********_____ ");
+			System.out.printf("\n ____*******_________________*******____ ");
+			System.out.printf("\n ___******______________________*****___ ");
+			System.out.printf("\n __****___________________________****__ ");
+			System.out.printf("\n _**_________________________________**_ ");
+			System.out.printf("\n\n               VOCÊ VENCEU!");
+		}
+	}	
+}
